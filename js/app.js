b901ec17f08d2f9fb19d2fa43d282e3f6102df26
gordonpm <gordonm0815@gmail.com>
2019-06-28 19:53:16 -0700

    updates

--- js/app.js
+++ js/app.js
@@ -36,3 +36,14 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ const cards = document.querySelectorAll(".card");
+
+ cards.forEach(card => {
+    card.addEventListener("click", function() {
+        console.log("card clicked");
+        card.classList.toggle('open');
+        card.classList.toggle('show');
+    });
+ });
+ 
