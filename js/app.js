76ed8d7f9efab6c138c911135bbe71441981b766
gordonpm <gordonm0815@gmail.com>
2019-07-04 18:53:21 -0700

    added comments

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
-/*
- * Create a list that holds all of your cards
- */
 const icons = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-bomb", "fa-leaf", "fa-bicycle"];
 var moves = 0;
 var countMatches = 0;
 var rating = 3;
 var totalSeconds = 0;
 var iconArray = [];
-var turn = [];
+var turn = []; // will hold the pair of cards in a move
 
 const deck = document.querySelector(".deck");
 const movesSpan = document.querySelector(".moves");
@@ -28,7 +25,7 @@ function initialize() {
     // add the listener to the deck which in turn listens to card clicks
     deck.addEventListener("click", respondToCardClick);
 
-    var playBtn = document.querySelector(".playBtn");
+    let playBtn = document.querySelector(".playBtn");
     playBtn.addEventListener("click", function(){
         document.querySelector(".modal").style.display = "none";
         totalSeconds = 0;
@@ -37,7 +34,7 @@ function initialize() {
         startGame();
     });
 
-    var restartBtn = document.querySelector(".restart");
+    let restartBtn = document.querySelector(".restart");
     restartBtn.addEventListener("click", function(){
         totalSeconds = 0;
         stopTimer();
@@ -47,7 +44,10 @@ function initialize() {
 }
 
 /**
- * This function will be called when a new game begins.
+ * This function will be called when a new game begins. Performs the following:
+ * 1) remove cards from previous game
+ * 2) add deck of shuffled cards
+ * 3) reset moves counter, match counter and rating
  */
 function startGame() {
     moves = 0;
@@ -77,7 +77,7 @@ function startGame() {
  * After all cards are matched, it will display modal.
  */
 function respondToCardClick(event) {
-    var card = event.target;
+    let card = event.target;
     if (moves === 0 && turn.length === 0) { // start timer after first card is clicked
         intervalId = setInterval(setTime, 1000);
     }
@@ -133,7 +133,7 @@ function getCardImage(card) {
  * Shuffle function from http://stackoverflow.com/a/2450976
  */
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -213,7 +213,6 @@ function updateRating() {
         stars.removeChild(stars.children[2]);
 
         // append li with empty star icon
-        // <li><i class="fa fa-star"></i></li>
         liElement = document.createElement("li");
         liElement.classList.add("fa", "fa-star-o");
         stars.appendChild(liElement);
@@ -247,12 +246,16 @@ function makeClickable(turn) {
 }
 
 function applyMatchStyle(turn) {
-    turn[0].classList.add("match");
-    turn[1].classList.add("match");
+    for (card of turn) {
+        card.classList.add("match");
+    }
 }
 
+/**
+ * This function will pad '0' to sec or min value if needed.
+ */
 function pad(val) {
-    var valString = val.toString();
+    let valString = val.toString();
     if (valString.length < 2) {
       return "0" + valString;
     } else {
